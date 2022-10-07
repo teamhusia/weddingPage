a3cf55f1961b28e67605b6c92a736a116a261a6f
Julia Bielecka <67067000+juliabiel25@users.noreply.github.com>
2022-10-07 15:49:09 +0200

    svgs update

--- teamhusia/src/components/LanguageControl/components/SvgPL.jsx
+++ teamhusia/src/components/LanguageControl/components/SvgPL.jsx
@@ -6,11 +6,20 @@ const SvgPL = (width, height) => {
       xmlns="http://www.w3.org/2000/svg"
       width="100%"
       height="100%"
-      style={{ borderRadius: "50%" }}
       viewBox="0 0 512 512"
     >
-      <path fill="#d80027" d="m0 256 256.4-44.3L512 256v256H0z" />
-      <path fill="#fff" d="M0 0h512v256H0z" />
+      <defs>
+        <clipPath id="cut-circle">
+          <circle cx={256} cy={256} r={256} />
+        </clipPath>
+      </defs>
+
+      <path
+        fill="#d80027"
+        d="m0 256 256.4-44.3L512 256v256H0z"
+        clipPath="url(#cut-circle)"
+      />
+      <path fill="#fff" d="M0 0h512v256H0z" clipPath="url(#cut-circle)" />
     </svg>
   );
 };
