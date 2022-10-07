a34879136dbd8d4ca035ce12c5f67442dbb9be26
Julia Bielecka <67067000+juliabiel25@users.noreply.github.com>
2022-10-07 15:12:45 +0200

    language control adjustments

---
+++ teamhusia/src/components/Invitation/components/HeaderImage.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const StyledDiv = styled.div`
+  margin: 2em;
+  width: 300px;
+  height: 300px;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+const HeaderImage = ({src, alt}) => {    
+  return (
+    <StyledDiv>
+      <img src={src} alt={alt} width={300} height={300}/>
+    </StyledDiv>
+  )
+}
+
+export default HeaderImage;
--- teamhusia/src/components/LanguageControl/index.jsx
+++ teamhusia/src/components/LanguageControl/index.jsx
@@ -1,61 +1,49 @@
-import SvgGB from './components/SvgGB';
-import SvgKR from './components/SvgKR';
-import SvgPL from './components/SvgPL';
-import { colors } from '../../theme/colors';
-import styled from "styled-components";
+import "./LanguageControl.css";
+
+import SvgGB from "./components/SvgGB";
+import SvgKR from "./components/SvgKR";
+import SvgPL from "./components/SvgPL";
 import { useTranslation } from "react-i18next";
 
-const Container = styled.div`
-  /* background-color: ${colors.lightNavy}; */
-  width: fit-content;
-  display: flex;
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  padding: 2em 0;
-  z-index: 5;
-`;
-
-const StyledButton = styled.button`
-  background-color: rgba(0, 0, 0, 0);
-  border: none;
-  height: 3em; 
-`;
+// const Container = styled.div`
+//   /* background-color: ${colors.lightNavy}; */
+//   width: fit-content;
+//   display: flex;
+//   align-items: center;
+//   align-content: center;
+//   justify-content: center;
+// `;
 
 const LanguageControl = () => {
   const { t, i18n } = useTranslation();
 
   return (
-    <Container>
-
-      <StyledDiv>
-        <StyledButton
-          onClick={() => {
-            i18n.changeLanguage("kr");
-          }}>
-            <SvgKR/>
-        </StyledButton>
-        <StyledButton
-          onClick={() => {
-            i18n.changeLanguage("pl");
-          }}>
-            <SvgPL/>
-        </StyledButton>
-        <StyledButton
-          onClick={() => {
-            i18n.changeLanguage("en");
-          }}>
-            <SvgGB/>
-        </StyledButton>        
-      </StyledDiv>
-      
-    </Container>
+    <div className="language-control">
+      <button
+        className="icon-button"
+        onClick={() => {
+          i18n.changeLanguage("kr");
+        }}
+      >
+        <SvgKR />
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          i18n.changeLanguage("pl");
+        }}
+      >
+        <SvgPL />
+      </button>
+      <button
+        className="icon-button"
+        onClick={() => {
+          i18n.changeLanguage("en");
+        }}
+      >
+        <SvgGB />
+      </button>
+    </div>
   );
 };
 
