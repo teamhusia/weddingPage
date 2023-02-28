3ddc5f99e0c3a90a956adf541dc025dc01b38351
Julia Bielecka <julia.bielecka@bbbit.io>
2023-02-28 23:20:22 +0100

    roadmap updates

--- teamhusia/src/components/Roadmap/index.jsx
+++ teamhusia/src/components/Roadmap/index.jsx
@@ -10,64 +10,116 @@ import { useTranslation } from "react-i18next";
 
 const Roadmap = ({}) => {
   const { t, i18n } = useTranslation();
+  console.log(i18n.language);
 
   return (
     <section id="section-roadmap" className="min-fullscreen scroll-target">
       <div className="roadmap">
-        <BrowserView>
-          <Board>
-            <Card>
-              <article>
-                <h1>{t("CEREMONY_LOC_TITLE")}</h1>
-                <Image
-                  src="/R6__8443_Fara_landscape_cut2.jpg"
-                  width="100%"
-                  height="14em"
-                />
-                <div className="roadmap-content">
-                  <p>{t("CEREMONY_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/HUeqLMmVau3j2xDH7" />
-                </div>
-              </article>
-            </Card>
-          </Board>
-          <Board>
-            <Card>
-              <article>
-                <h1>{t("CELEBRATION_LOC_TITLE")}</h1>
-                <Image
-                  src="/Stary_kamionek_cut1.jpg"
-                  width="100%"
-                  height="14em"
-                />
-                <div className="roadmap-content">
-                  <p>{t("CELEBRATION_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/wBc6bgRYD93waBHe8" />
-                </div>
-              </article>
-            </Card>
-          </Board>
-          <div className="last">
-            <Board>
-              <Card>
-                <article>
-                  <h1>{t("ACCOMODATION_LOC_TITLE")}</h1>
-                  <Image src="/hotel.jpg" width="100%" height="14em" />
-                  <div className="roadmap-content">
-                    <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
-                    <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
-                  </div>
-                </article>
-              </Card>
-            </Board>
-          </div>
-        </BrowserView>
+        <Board>
+          <Card>
+            <div>
+              <h1>{t("ROADMAP")}</h1>
+            </div>
+            <BrowserView>
+              <img
+                src="/map-wide-opt.png"
+                width="100%"
+                alt="A map showing the main landmarks of the event"
+              />
 
-        <MobileView>
-          <Board>
-            <Card>
               <article>
-                <h1>{t("CEREMONY_LOC_TITLE")}</h1>
+                <h2>{t("CEREMONY_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("CEREMONY_LOC_SUBTITLE")}
+                </h3>
+                <Image
+                  src="/R6__8443_Fara_landscape_cut2.jpg"
+                  width="100%"
+                  height="14em"
+                />
+                <div className="roadmap-content">
+                  <p>{t("CEREMONY_LOC_DESCRIPTION")}</p>
+                  <GoogleMapsLink url="https://goo.gl/maps/HUeqLMmVau3j2xDH7" />
+                </div>
+              </article>
+
+              <article>
+                <h2>{t("CELEBRATION_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("CELEBRATION_LOC_SUBTITLE")}
+                </h3>
+                <Image
+                  src="/Stary_kamionek_cut1.jpg"
+                  width="100%"
+                  height="14em"
+                />
+                <div className="roadmap-content">
+                  <p>{t("CELEBRATION_LOC_DESCRIPTION")}</p>
+                  <GoogleMapsLink url="https://goo.gl/maps/wBc6bgRYD93waBHe8" />
+                </div>
+              </article>
+
+              {i18n.language !== "pl" && (
+                <article>
+                  <h2>{t("KASIAS_HOUSE_TITLE")}</h2>
+                  <h3 className="roadmap-subtitle">
+                    {t("KASIAS_HOUSE_SUBTITLE")}
+                  </h3>
+                  <Image src="/morszynska-cut.jpg" width="100%" height="14em" />
+                  <div className="roadmap-content">
+                    <p>{t("KASIAS_HOUSE_DESCRIPTION")}</p>
+                    <GoogleMapsLink url="https://goo.gl/maps/zprAcdU9XbKxdsWdA" />
+                  </div>
+                </article>
+              )}
+
+              <article>
+                <h2>{t("ACCOMODATION_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("ACCOMODATION_LOC_SUBTITLE")}
+                </h3>
+                <Image src="/hotel.jpg" width="100%" height="14em" />
+                <div className="roadmap-content">
+                  <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
+                  <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
+                </div>
+              </article>
+
+              {i18n.language !== "pl" && (
+                <article>
+                  <h2>{t("AIRPORT_LOC_TITLE")}</h2>
+                  <div className="roadmap-content">
+                    <p>
+                      {t("AIRPORT_LOC_DESCRIPTION")} <br />
+                      {t("AIRPORT_FLIGHTS_TEXT")}
+                    </p>
+                    <div className="row">
+                      <img
+                        src="flights-1.jpg"
+                        width="300px"
+                        style={{ borderRadius: "1em" }}
+                        alt="calendar showing possible flights from the UK"
+                      />
+                      <img
+                        src="flights-2.png"
+                        width="300px"
+                        style={{ borderRadius: "1em" }}
+                        alt="calendar showing possible flights from the UK"
+                      />
+                    </div>
+                    <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
+                  </div>
+                </article>
+              )}
+            </BrowserView>
+
+            <MobileView>
+              <Image src="/map-narrow-opt.png" width="100%" height="14em" />
+              <article>
+                <h2>{t("CEREMONY_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("CEREMONY_LOC_SUBTITLE")}
+                </h3>
                 <Image
                   src="/R6__8443_Fara_landscape_cut2.jpg"
                   width="100%"
@@ -78,12 +130,11 @@ const Roadmap = ({}) => {
                   <GoogleMapsLink url="https://goo.gl/maps/HUeqLMmVau3j2xDH7" />
                 </div>
               </article>
-            </Card>
-          </Board>
-          <Board>
-            <Card>
               <article>
-                <h1>{t("CELEBRATION_LOC_TITLE")}</h1>
+                <h2>{t("CELEBRATION_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("CELEBRATION_LOC_SUBTITLE")}
+                </h3>
                 <Image
                   src="/Stary_kamionek_cut1.jpg"
                   width="100%"
@@ -94,23 +145,73 @@ const Roadmap = ({}) => {
                   <GoogleMapsLink url="https://goo.gl/maps/wBc6bgRYD93waBHe8" />
                 </div>
               </article>
-            </Card>
-          </Board>
-          <div className="last">
-            <Board>
-              <Card>
+              <article>
+                <h2>{t("ACCOMODATION_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("ACCOMODATION_LOC_SUBTITLE")}
+                </h3>
+                <Image src="/hotel.jpg" width="100%" height="9em" />
+                <div className="roadmap-content">
+                  <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
+                  <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
+                </div>
+              </article>
+
+              {i18n.language !== "pl" && (
                 <article>
-                  <h1>{t("ACCOMODATION_LOC_TITLE")}</h1>
-                  <Image src="/hotel.jpg" width="100%" height="9em" />
+                  <h2>{t("KASIAS_HOUSE_TITLE")}</h2>
+                  <h3 className="roadmap-subtitle">
+                    {t("KASIAS_HOUSE_SUBTITLE")}
+                  </h3>
+                  <Image src="/morszynska-cut.jpg" width="100%" height="14em" />
                   <div className="roadmap-content">
-                    <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
+                    <p>{t("KASIAS_HOUSE_DESCRIPTION")}</p>
+                    <GoogleMapsLink url="https://goo.gl/maps/zprAcdU9XbKxdsWdA" />
+                  </div>
+                </article>
+              )}
+
+              <article>
+                <h2>{t("ACCOMODATION_LOC_TITLE")}</h2>
+                <h3 className="roadmap-subtitle">
+                  {t("ACCOMODATION_LOC_SUBTITLE")}
+                </h3>
+                <Image src="/hotel.jpg" width="100%" height="14em" />
+                <div className="roadmap-content">
+                  <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
+                  <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
+                </div>
+              </article>
+
+              {i18n.language !== "pl" && (
+                <article>
+                  <h2>{t("AIRPORT_LOC_TITLE")}</h2>
+                  <div className="roadmap-content">
+                    <p>
+                      {t("AIRPORT_LOC_DESCRIPTION")} <br />
+                      {t("AIRPORT_FLIGHTS_TEXT")}
+                    </p>
+                    <div>
+                      <img
+                        src="flights-1.jpg"
+                        width="300px"
+                        style={{ borderRadius: "1em" }}
+                        alt="calendar showing possible flights from the UK"
+                      />
+                      <img
+                        src="flights-2.png"
+                        width="300px"
+                        style={{ borderRadius: "1em" }}
+                        alt="calendar showing possible flights from the UK"
+                      />
+                    </div>
                     <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
                   </div>
                 </article>
-              </Card>
-            </Board>
-          </div>
-        </MobileView>
+              )}
+            </MobileView>
+          </Card>
+        </Board>
       </div>
     </section>
   );
--- teamhusia/src/i18n/translations/pl.js
+++ teamhusia/src/i18n/translations/pl.js
@@ -11,22 +11,30 @@ export const pl = {
     INVITATION_PLACE_2:
       "Po ceremonii zapraszamy na przyjęcie weselne,\nktóre odbędzie się w Starym Kamionku.",
     INVITATION_RSVP_BUTTON: "RSVP",
+
     CEREMONY_LOC_TITLE: "Ceremonia",
+    CEREMONY_LOC_SUBTITLE: "",
     CEREMONY_LOC_DESCRIPTION:
       "Nasza ceremonia Kościelna odbędzie sie w Farze Poznańskiej, leżącej w samym sercu Starego Miasta Poznania. Po cermoni autokar zawiezie naszych gości na miejsce przyjęcia.",
+
     CELEBRATION_LOC_TITLE: "Przyjęcie weselne",
+    CELEBRATION_LOC_SUBTITLE: "",
     CELEBRATION_LOC_DESCRIPTION:
       "Po Mszy Świętej zapraszamy naszych Gości na przyjęcie Weselne w Starym Kamionku.",
+
     ACCOMODATION_LOC_TITLE: "Nocleg",
+    ACCOMODATION_LOC_SUBTITLE: "",
     ACCOMODATION_LOC_DESCRIPTION:
       "Jeśli szukasz hotelu w Poznaniu, polecamy Hotel Ilonn na podolanach. Są też dostępne pokoje hotelowe w Starym Kamionku na noc przyjęcia. Pokój dwuosobowy to koszt ok. 220zł, dodatkowo śniadanie 40zł/os. Zainteresowanych prosimy o kontakt.",
+
     MENU_HOME: "ZAPROSZENIE",
-    MENU_ROADMAP: "MAPA WYDARZEŃ",
+    MENU_ROADMAP: "MAPA WYDARZENIA",
     MENU_TEAM: "TEAM",
     MENU_CONTACT: "KONTAKT",
     MENU_RSVP: "RSVP",
     GOOGLE_MAPS_BTN: "POKAŻ NA MAPIE",
     Contact: "Kontakt",
-    RSVP_BUTTON_LABEL: "RSVP"
+    RSVP_BUTTON_LABEL: "RSVP",
+    ROADMAP: "Mapa wydarzenia"
   }
 };
