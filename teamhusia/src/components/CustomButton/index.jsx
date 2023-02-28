c865688951ce27c186fee627f9c4a32d73e48230
juliabiel25 <juliabiel25@gmail.com>
2023-02-28 11:17:29 +0100

    button corrections

---
+++ teamhusia/src/components/CustomButton/index.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+
+const CustomButton = ({ children, ...buttonProps }) => {
+  return (
+    <Button
+      {...buttonProps}
+      variant="outlined"
+      size="large"
+      className="custom-btn"
+      color="primary"
+      sx={{ borderRadius: "1em", margin: "0 1em" }}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default CustomButton;
