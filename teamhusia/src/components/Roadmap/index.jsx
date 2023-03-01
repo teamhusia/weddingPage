3daad82b2756fe2704f79d649f8f37b0e7b44df6
juliabiel25 <juliabiel25@gmail.com>
2023-03-01 10:42:44 +0100

    general improvements

--- teamhusia/src/components/Roadmap/index.jsx
+++ teamhusia/src/components/Roadmap/index.jsx
@@ -1,11 +1,10 @@
 import "./Roadmap.css";
 
-import { BrowserView, MobileView, isBrowser } from "react-device-detect";
-
 import Board from "../Board";
 import Card from "../Card";
 import GoogleMapsLink from "../GoogleMapsLink";
 import Image from "./components/Image";
+import { isBrowser } from "react-device-detect";
 import { useTranslation } from "react-i18next";
 
 const Roadmap = ({}) => {
@@ -24,9 +23,8 @@ const Roadmap = ({}) => {
               src={`${isBrowser ? "/map-wide-opt.png" : "/map-narrow-opt.png"}`}
               width="100%"
               alt="A map showing the main landmarks of the event"
+              className="img-with-border"
             />
-
-            <div className="divider"></div>
 
             <article>
               <div className="roadmap-header">
@@ -46,8 +44,6 @@ const Roadmap = ({}) => {
               </div>
             </article>
 
-            <div className="divider"></div>
-
             <article>
               <h2>{t("CELEBRATION_LOC_TITLE")}</h2>
               <h3 className="roadmap-subtitle">
@@ -64,8 +60,6 @@ const Roadmap = ({}) => {
               </div>
             </article>
 
-            <div className="divider"></div>
-
             {i18n.language !== "pl" && i18n.language !== "pl-PL" && (
               <>
                 <article>
@@ -79,8 +73,6 @@ const Roadmap = ({}) => {
                     <GoogleMapsLink url="https://goo.gl/maps/zprAcdU9XbKxdsWdA" />
                   </div>
                 </article>
-
-                <div className="divider"></div>
               </>
             )}
 
@@ -101,8 +93,6 @@ const Roadmap = ({}) => {
                 <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
               </div>
             </article>
-
-            <div className="divider"></div>
 
             {i18n.language !== "pl" && i18n.language !== "pl-PL" && (
               <>
@@ -131,129 +121,8 @@ const Roadmap = ({}) => {
                     <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
                   </div>
                 </article>
-
-                <div className="divider"></div>
               </>
             )}
-            {/* </BrowserView> */}
-
-            {/* <MobileView>
-              <Image src="/map-narrow-opt.png" width="100%" height="14em" />
-              <article>
-                <h2>{t("CEREMONY_LOC_TITLE")}</h2>
-                <h3 className="roadmap-subtitle">
-                  {t("CEREMONY_LOC_SUBTITLE")}
-                </h3>
-                <Image
-                  src="/R6__8443_Fara_landscape_cut2.jpg"
-                  width="100%"
-                  height="9em"
-                />
-                <div className="roadmap-content">
-                  <p>{t("CEREMONY_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/HUeqLMmVau3j2xDH7" />
-                </div>
-              </article>
-
-              <div className="divider"></div>
-              <article>
-                <h2>{t("CELEBRATION_LOC_TITLE")}</h2>
-                <h3 className="roadmap-subtitle">
-                  {t("CELEBRATION_LOC_SUBTITLE")}
-                </h3>
-                <Image
-                  src="/Stary_kamionek_cut1.jpg"
-                  width="100%"
-                  height="9em"
-                />
-                <div className="roadmap-content">
-                  <p>{t("CELEBRATION_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/wBc6bgRYD93waBHe8" />
-                </div>
-              </article>
-
-              <div className="divider"></div>
-              <article>
-                <h2>{t("ACCOMODATION_LOC_TITLE")}</h2>
-                <h3 className="roadmap-subtitle">
-                  {t("ACCOMODATION_LOC_SUBTITLE")}
-                </h3>
-                <Image src="/hotel.jpg" width="100%" height="9em" />
-                <div className="roadmap-content">
-                  <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
-                </div>
-              </article>
-
-              <div className="divider"></div>
-
-              {i18n.language !== "pl" && (
-                <>
-                  <article>
-                    <h2>{t("KASIAS_HOUSE_TITLE")}</h2>
-                    <h3 className="roadmap-subtitle">
-                      {t("KASIAS_HOUSE_SUBTITLE")}
-                    </h3>
-                    <Image
-                      src="/morszynska-cut.jpg"
-                      width="100%"
-                      height="14em"
-                    />
-                    <div className="roadmap-content">
-                      <p>{t("KASIAS_HOUSE_DESCRIPTION")}</p>
-                      <GoogleMapsLink url="https://goo.gl/maps/zprAcdU9XbKxdsWdA" />
-                    </div>
-                  </article>
-
-                  <div className="divider"></div>
-                </>
-              )}
-
-              <article>
-                <h2>{t("ACCOMODATION_LOC_TITLE")}</h2>
-                <h3 className="roadmap-subtitle">
-                  {t("ACCOMODATION_LOC_SUBTITLE")}
-                </h3>
-                <Image src="/hotel.jpg" width="100%" height="14em" />
-                <div className="roadmap-content">
-                  <p>{t("ACCOMODATION_LOC_DESCRIPTION")}</p>
-                  <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
-                </div>
-              </article>
-
-              <div className="divider"></div>
-
-              {i18n.language !== "pl" && (
-                <>
-                  <article>
-                    <h2>{t("AIRPORT_LOC_TITLE")}</h2>
-                    <div className="roadmap-content">
-                      <p>
-                        {t("AIRPORT_LOC_DESCRIPTION")} <br />
-                        {t("AIRPORT_FLIGHTS_TEXT")}
-                      </p>
-                      <div>
-                        <img
-                          src="flights-1.jpg"
-                          width="300px"
-                          style={{ borderRadius: "1em" }}
-                          alt="calendar showing possible flights from the UK"
-                        />
-                        <img
-                          src="flights-2.png"
-                          width="300px"
-                          style={{ borderRadius: "1em" }}
-                          alt="calendar showing possible flights from the UK"
-                        />
-                      </div>
-                      <GoogleMapsLink url="https://goo.gl/maps/ZthM6nBdzNFefsbT7" />
-                    </div>
-                  </article>
-
-                  <div className="divider"></div>
-                </>
-              )}
-            </MobileView> */}
           </Card>
         </Board>
       </div>
